ee1dad9e6641e9d1c566ef24276d9ca7adad8a73
Clara <clara.laviale@gmail.com>
2025-01-17 09:22:52 +0100

    modify class to abstract

--- Java/TPVisiteMedecin/src/bo/Medecin.java
+++ Java/TPVisiteMedecin/src/bo/Medecin.java
@@ -3,8 +3,8 @@ package bo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Medecin extends Personne {
-	private List<Creneau> creneaux = new ArrayList<Creneau>();
+public abstract class Medecin extends Personne {
+	protected List<Creneau> creneaux = new ArrayList<Creneau>();
 	
 	// constructeur 
 	public Medecin(String lastName, String firstName, String phoneNumber, Adresse adress) {
@@ -14,9 +14,7 @@ public class Medecin extends Personne {
 	// methodes
 	
 	// La méthode recupererTarif n'a pas de sens dans la classe Medecin : elle renvoie donc une valeur absurde
-	public int recupererTarif() {
-		throw new RuntimeException("On ne devrait pas passer par là");
-	}
+	public abstract int recupererTarif();
 		
 	public void ajouterCreneau(Creneau creneauAAjouter) {
 		creneaux.add(creneauAAjouter);
