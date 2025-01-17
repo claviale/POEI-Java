b80d8272269a8393482700841eefc2eee4381c19
Clara <clara.laviale@gmail.com>
2025-01-17 09:45:14 +0100

    demo abstraction

---
+++ Java/DemoPOO/src/abstraction/Oiseau.java
@@ -0,0 +1,35 @@
+package abstraction;
+
+public class Oiseau extends Animal {
+	private boolean volant;
+	
+	public Oiseau(String nom, boolean volant) {
+		super(nom);
+		this.volant = volant;
+	}
+
+	@Override
+	public String crier() {
+		return "Cui cui";
+	}
+	
+	public boolean isVolant() {
+		return volant;
+	}
+
+	public void setVolant(boolean volant) {
+		this.volant = volant;
+	}
+	
+	@Override
+	public String toString() {
+		String result = super.toString();
+		result += "\n\t";
+		if (volant) {
+			result += "Est capable de voler";
+		} else {
+			result += "Ne peut pas voler";
+		}
+		return result;
+	}
+}
