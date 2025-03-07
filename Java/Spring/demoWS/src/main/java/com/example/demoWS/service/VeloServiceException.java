8ff5bb32881c5b02ecf0da1aa2472f36a55a9969
Clara <clara.laviale@gmail.com>
2025-03-07 16:53:28 +0100

    some spring demo

---
+++ Java/Spring/demoWS/src/main/java/com/example/demoWS/service/VeloServiceException.java
@@ -0,0 +1,11 @@
+package com.example.demoWS.service;
+
+public class VeloServiceException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public VeloServiceException(String message) {
+		super(message);
+	}
+
+}
